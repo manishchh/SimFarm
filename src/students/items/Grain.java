48a8fa97a2b78a69fbd62ea104552da11856d848
manishchh <81131446+manishchh@users.noreply.github.com>
2023-03-22 04:21:57 +1030

    Update Grain.java
    Implemented class constructor, defined variable & methods

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -1,6 +1,24 @@
 package students.items;
 
-public class Grain{
-
+public class Grain extends Food{
 	
+	private static int numberOfInstances = 0;
+	private static final int grainCost = 1;
+	
+	Grain(){
+		super(2,6,2);
+	}
+	
+	public int getGenerationCount() {
+		return Grain.numberOfInstances;
+	}
+
+	public String toString() {
+		if(this.age < this.maturationAge) 
+		{
+			return "g";
+		}
+		
+		return "G";
+	}
 }
