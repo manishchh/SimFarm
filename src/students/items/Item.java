137de5fb7d65a067f6ccdf876871cc14f1d226df
manishchh <81131446+manishchh@users.noreply.github.com>
2023-03-22 03:49:56 +1030

    Update Item.java
    Logic & comments for each method of item class

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -7,21 +7,88 @@ package students.items;
  * Every item tracks its age, maturation age, death age, and montery value.
  * All items start at age 0, with the maturation age, death age, and monetary value set as part of its constructor. 
  */
+
 public class Item {
 	int age;
 	int maturationAge;
 	int deathAge;
-	int monteryValue;
+	int monetaryValue;
 	
-	Item(int maturationAge, int deathAge, int monteryValue){
+	/**
+	 * Defining constructor of item class 
+	 */
+	Item(int maturationAge, int deathAge, int monetaryValue){
 		this.age = 0;
 		this.maturationAge = maturationAge;
 		this.deathAge = deathAge;
-		this.monteryValue = monteryValue;
+		this.monetaryValue = monetaryValue;
 		
 	}
+	/**
+	 * Age increases by 1
+	 */
+	public void tick() {
+		this.age++;
+	}
+	
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	/**
+	 * to check whether item has died
+	 * @return  true if age > deathAge else false
+	 */
+	public boolean died() {
+		return this.age > this.deathAge;
+		
+	}
+	
+	
+	/**
+	 *  Value is returned if food item age is >= maturation age else 
+	 *  return 0 if item is not matured 
+	 */
+	public int getValue() {
+		if (this.age >= this.maturationAge) {
+			return this.maturationAge;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	
+	/**
+	 * check if food items age is equal to other object.
+	 * 
+	 * @return true if object equal else false
+	 *
+	 */
+	public boolean equals(Object obj) {
+		Item other = (Item) obj;
+		if(other == null) {
+			return false;
+		}
+		if (this.age == other.age && this.deathAge == other.deathAge && this.maturationAge == other.maturationAge && this.monetaryValue == other.monetaryValue) {
+			return true;
+		}
+			
+		return false;
+		
+	}
+	
 
 	
+
 	
-	
-}
+	/*
+	 * Implementation of abstract method to instantiate subclasses which returns string representation of item class
+	 */
+	public abstract String toString();
+}	
+
