208e569a654ae6913025da42c65205fb1762265c
manishchh <81131446+manishchh@users.noreply.github.com>
2023-03-22 04:07:06 +1030

    Update Food.java
    implementing abstract class, constructor and comments

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,6 +1,16 @@
 package students.items;
 
-public class Food {
-
+/**
+ * 
+ * @author manish chhetri
+ * Food class is abstract because it cannot be instantiated
+ * Food class is an item for which it extends from item class
+ */
+public abstract class Food extends Item{
+	// constructor of food class
+	Food(int maturationAge, int deathAge, int monetaryValue){
+		super(maturationAge, deathAge, monetaryValue);
+		
+	}
 
 }
