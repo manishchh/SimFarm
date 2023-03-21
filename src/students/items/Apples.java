a250de53635616cabb89de00cdde9b395cc30f7f
manishchh <81131446+manishchh@users.noreply.github.com>
2023-03-22 04:30:48 +1030

    Update Apples.java
    Implemented class constructor, defined variable & methods

--- src/students/items/Apples.java
+++ src/students/items/Apples.java
@@ -1,5 +1,27 @@
 package students.items;
 
-public class Apples {
-
+public class Apples extends Food {
+	
+	private static int numberOfInstances = 0;
+	private static final int appleCost = 2;
+	Apples(){
+		super(3,5,3);
+		
+	}
+	
+	public int getGenerationCount() {
+		return Apples.numberOfInstances;
+	}
+	public String toString() {
+		if (this.age < this.maturationAge)
+		{
+			return "a";
+			
+		}
+		return "A";
+	}
+	
+	public int getCost() {
+		return appleCost;
+	}
 }
