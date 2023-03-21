11cf52100f3f64ea1f32aa3ede4d732098115b99
manishchh <81131446+manishchh@users.noreply.github.com>
2023-03-22 03:09:53 +1030

    Update Item.java
    Initialized variables & constructor for item class

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,27 @@
 package students.items;
 
+/**
+ * 
+ * @author manish chhetri
+ * Item is the base class of all farm items. 
+ * Every item tracks its age, maturation age, death age, and montery value.
+ * All items start at age 0, with the maturation age, death age, and monetary value set as part of its constructor. 
+ */
 public class Item {
+	int age;
+	int maturationAge;
+	int deathAge;
+	int monteryValue;
+	
+	Item(int maturationAge, int deathAge, int monteryValue){
+		this.age = 0;
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+		this.monteryValue = monteryValue;
+		
+	}
 
+	
+	
 	
 }
