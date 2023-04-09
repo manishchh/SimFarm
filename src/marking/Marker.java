319f22ec776e63554965a37848dc612861c31931
manishchh <81131446+manishchh@users.noreply.github.com>
2023-04-09 15:16:37 +0930

    Update Marker.java
    added CloneNotSUpportedException in main method because of implementation of clone() method

--- src/marking/Marker.java
+++ src/marking/Marker.java
@@ -6,8 +6,8 @@ import students.items.*;
 
 public class Marker {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws CloneNotSupportedException{
+
 		// Section ONE
 		
 		//you should NOT be able to make an Item - following line shouldn't compile if uncommented
@@ -29,11 +29,10 @@ public class Marker {
 
 		System.out.println(new Weed()); // should print "#"
 		System.out.println(new UntilledSoil()); // should print "/"
-//
-		// Section TWO
-		// uncomment this section once you get to it
+
 		
-		/*
+		// Section TWO
+		
 		Field testField = new Field(5, 5);
 		System.out.println(testField.get(3, 3)); //should print "."
 		System.out.println(testField); //should print the field
@@ -46,7 +45,7 @@ public class Marker {
 		testField.till(0, 0);
 		System.out.println(a.equals(testField.get(0, 0))); //should print false
 		System.out.println(testField.get(0, 0).equals(new Soil())); //should print true
-		*/
+		
 
 		// Section THREE
 		// uncomment this section once you get to it
