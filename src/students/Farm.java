d0beff71ebd5a838aa0010dabe69820976ef7631
manishchh <81131446+manishchh@users.noreply.github.com>
2023-04-09 19:26:04 +0930

    player invalid input
    checks if player has entered the choice or not

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -28,6 +28,8 @@ public class Farm {
 		Scanner sc = new Scanner(System.in);
 		String player_input;
 		while(true){
+			
+			//display the current balance and field state
 			System.out.println(field.toString() + "\n");
 			System.out.println("Bank balance: $" + startingFunds + "\n");
 			System.out.println("Enter your next action:");
@@ -38,9 +40,20 @@ public class Farm {
 			System.out.println("w: wait");
 			System.out.println("q: quit");
 			
+			//ask user choice of input
 			player_input = sc.nextLine().trim();
 			String[] inputs = player_input.split("\\s+");
 			
+			/**
+			 * check the length of player input is equal to 0 
+			 * if 0 ask player to enter the choice again
+			 */
+			
+			if(inputs.length == 0) {
+				System.out.println("Invalid input. Try again.");
+				continue;
+				
+			}
 			
 			
 			
